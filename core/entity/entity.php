d522cbd45f6016129ddf5207c136b580f20a7262
Eric Höglander <erichoglander@gmail.com>
2015-05-07 20:41:36 +0200

    Basic user entity

--- core/entity/entity.php
+++ core/entity/entity.php
@@ -1,16 +1,69 @@
 <?php
 class Entity {
-	
+
+	protected $fields;
+	protected $schema;
 	protected $Db;
 
 	public function __construct(&$Db, $id = null) {
 		$this->Db = $Db;
+		$this->schema = $this->schema();
 		if ($id)
 			$this->load($id);
 	}
 
-	public function load($id) {
+	public function id() {
+		if (empty($this->fields['id']))
+			return null;
+		return (int) $this->fields['id'];
+	}
 
+	public function get($field) {
+		if (!array_key_exists($field, $this->fields))
+			return null;
+		$value = $this->$fields[$field];
+		if (!empty($this->schema[$field]['serialize'])
+			$value = unserialize($value);
+		if (!empty($this->schema[$field]['json'])
+			$value = json_decode($value);
+		return $value;
+	}
+
+	public function set($field, $value) {
+		if (!array_key_exists($field, $this->schema))
+			return null;
+		if (!empty($this->schema[$field]['serialize'])
+			$value = serialize($value);
+		if (!empty($this->schema[$field]['json'])
+			$value = json_encode($value);
+		if ($this->schema[$field]['type'] == "uint")
+			$value = abs((int) $value);
+		else if ($this->schema[$field]['type'] == "int")
+			$value = (int) $value;
+		$this->fields[$field] = $value;
+		return $value;
+	}
+
+	public function load($id) {
+		$row = $this->Db->getRow("SELECT * FROM `".$this->schema['table']."` WHERE id = :id", [":id" => $id]);
+		if (!$row)
+			return false;
+		foreach ($row as $key => $value) {
+			if (array_key_exists($key, $this->schema))
+				$this->fields[$key] = $value;
+		}
+		return true;
+	}
+
+	public function save() {
+		if ($this->id())
+			return $this->update($this->schema['table'], $this->fields, ["id" => $this->id()]);
+		else
+			return $this->fields['id'] = $this->insert($this->schema['table'], $this->fields);
+	}
+
+	public function delete() {
+		return $this->delete($this->schema['table'], ["id" => $this->id()]);
 	}
 
 
--- core/entity/user.php
+++ core/entity/user.php
@@ -1,6 +1,67 @@
 <?php
 class User_Core_Entity extends Entity {
+
+	public function save() {
+		if (substr($this->get("pass"), 0, 2) !== "1#") {
+			$this->fields['salt'] = $this->generateSalt();
+			$this->fields['pass'] = $this->hash($this->get("pass"), $this->get("salt"));
+		}
+		return parent::save();
+	}
+
+	public function logout() {
+		unset($_SESSION['user_id']);
+	}
+	public function login() {
+		$_SESSION['user_id'] = $this->id();
+	}
+
+
+	public function authorize($pass) {
+		return $this->hash($pass, $this->get("salt")) === $this->get("pass");
+	}
+
+	public function verifyResetLink($link) {
+		return $link === $this->hash($this->get("reset"), "qfresetlink");
+	}
+
+	public function generateResetLink() {
+		$reset = hash("sha512", microtime(true)."qfreset".rand(10001, 20000));
+		$link = $this->hash($reset, "qfresetlink");
+		$this->set("reset", $reset);
+		return $link;
+	}
+
+
+	protected function hash($pass, $salt) {
+		return "1#".hash("sha512", $salt.hash("sha512", $pass).hash("sha512", $salt."qfpass"));
+	}
+
+	protected function generateSalt() {
+		return hash("sha512", microtime(true).rand(1, 10000)."qfsalt");
+	}
 	
-	
-	
+	protected function schema() {
+		$schema = parent::schema();
+		$schema['table'] = "user";
+		$schema['fields']['name'] = [
+			"type" => "varchar",
+		];
+		$schema['fields']['email'] = [
+			"type" => "varchar",
+		];
+		$schema['fields']['login'] = [
+			"type" => "uint",
+		];
+		$schema['fields']['salt'] = [
+			"type" => "varchar",
+		];
+		$schema['fields']['pass'] = [
+			"type" => "varchar",
+		];
+		$schema['fields']['reset'] = [
+			"type" => "varchar",
+		];
+	}
+
 };
