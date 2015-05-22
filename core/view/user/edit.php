391f19a7ddd2334d9a5e6319fddcec4d814a3623
Eric Höglander <erichoglander@gmail.com>
2015-05-22 23:17:12 +0200

    fix breadcrumbs in user add/edit/delete

--- core/view/user/edit.php
+++ core/view/user/edit.php
@@ -1,6 +1,6 @@
 <?php
 $this->Html->h1 = $this->Html->title = t("Edit user");
-$this->Html->breadcrumbs[] = ["user/listing", t("Users")];
+$this->Html->breadcrumbs[] = ["user/list", t("Users")];
 $this->Html->breadcrumbs[] = $this->Html->title;
 
 print $form;
