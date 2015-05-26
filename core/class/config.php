b97a44bd39fe98c5b04ef25f9d5e18e3c52ea865
Eric Höglander <erichoglander@gmail.com>
2015-05-26 19:24:33 +0200

    Added ckeditor library

--- core/class/config.php
+++ core/class/config.php
@@ -93,7 +93,7 @@ class Config_Core {
 		return "closed";
 	}
 	public function getLibraries() {
-		return ["FontAwesome"];
+		return ["FontAwesome", "CKEditor"];
 	}
 	public function getAutomaticCron() {
 		return true;
