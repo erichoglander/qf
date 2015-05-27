3892c86591b8eeec4212ca58bf0662f9ae6aa7a3
= <eric@addcream.se>
2015-05-27 09:16:42 +0200

    IS_CLI was still true for hhvm is there was a query string

--- core/inc/constants.php
+++ core/inc/constants.php
@@ -4,7 +4,7 @@ define("PUBLIC_URI", "/files");
 define("PRIVATE_URI", "/file/private");
 define("PUBLIC_PATH", DOC_ROOT.PUBLIC_URI);
 define("PRIVATE_PATH", DOC_ROOT."/files/private");
-define("IS_CLI", !empty($_SERVER["argv"][0]));
+define("IS_CLI", !empty($_SERVER["shell"]));
 define("HTTP_PROTOCOL", (empty($_SERVER["HTTPS"]) ? "http" : "https"));
 define("BASE_DOMAIN", preg_replace("/^.*\.([^\.]+\.[^\.]+)$/", "$1", $_SERVER["SERVER_NAME"]));
 define("SITE_URL", HTTP_PROTOCOL."://".$_SERVER["HTTP_HOST"]);
