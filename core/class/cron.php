9619c6aab84f5a4eb710efcdc5e5b92b5de44f5e
= <eric@addcream.se>
2015-05-29 10:54:00 +0200

    Fixed deletion of temp files in cron

--- core/class/cron.php
+++ core/class/cron.php
@@ -20,7 +20,7 @@ class Cron_Core {
 					created < :time",
 				[":time" => REQUEST_TIME - 60*60*24]);
 		foreach ($rows as $row) {
-			$File = newClass("File", $this->Db, $row->id);
+			$File = newClass("File_Entity", $this->Db, $row->id);
 			$File->delete();
 			addlog($this->Db, "file", t("Deleted temporary file :name", "en", [":name" => $File->get("name")]), null, "success");
 		}
