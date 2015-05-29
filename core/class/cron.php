521f2ea7ac2398f31467d53d129b670d542b637e
= <eric@addcream.se>
2015-05-29 14:40:51 +0200

    Changed logging of temporary files cleanup

--- core/class/cron.php
+++ core/class/cron.php
@@ -19,11 +19,17 @@ class Cron_Core {
 					status = 0 && 
 					created < :time",
 				[":time" => REQUEST_TIME - 60*60*24]);
+		$deleted = [];
 		foreach ($rows as $row) {
 			$File = newClass("File_Entity", $this->Db, $row->id);
 			$File->delete();
-			addlog($this->Db, "file", t("Deleted temporary file :name", "en", [":name" => $File->get("name")]), null, "success");
+			$deleted[] = [
+				"name" => $File->get("name"),
+				"uri" => $File->get("uri"),
+			];
 		}
+		if (!empty($deleted)) 
+			addlog($this->Db, "file", t("Deleted :n temporary files", "en", [":n" => count($deleted)]), $deleted, "success");
 	}
 
 }
