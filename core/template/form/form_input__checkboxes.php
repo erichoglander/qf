d0611b65d5fc4324d6f62f63159051a8cb2c8b4e
Eric Höglander <erichoglander@gmail.com>
2015-06-09 11:49:43 +0200

    Fixed checkboxes input notice

--- core/template/form/form_input__checkboxes.php
+++ core/template/form/form_input__checkboxes.php
@@ -1,6 +1,6 @@
 <?php foreach ($options as $key => $val) { ?>
 	<label class="form-label checkbox-label">
-		<input <?=$attributes?> value="<?=$key?>"<?=(in_array($key, $value) ? " checked" : "")?>> 
+		<input <?=$attributes?> value="<?=$key?>"<?=($value && in_array($key, $value) ? " checked" : "")?>> 
 		<?=FontAwesome\Icon("square checkbox-icon-unchecked")?>
 		<?=FontAwesome\Icon("check-square checkbox-icon-checked")?> 
 		<?=$val?>
