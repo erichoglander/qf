6c8bb687596f9529e38d3ae8a220f14f07d83a13
= <eric@addcream.se>
2015-06-22 08:57:49 +0200

    Datepicker done

--- core/library/default/js/datepicker.js
+++ core/library/default/js/datepicker.js
@@ -28,6 +28,7 @@ function datepicker(el) {
 	this.now = new Date();
 	this.value = {y: this.now.getFullYear(), m: this.now.getMonth()+1, d: 0};
 	this.active = {y: this.value.y, m: this.value.m};
+	this.selectActive = null;
 	this.months = [
 		"January", "February", "March", 
 		"April", "May", "June", 
@@ -63,6 +64,8 @@ function datepicker(el) {
 		this.tags.next = document.createElement("div");
 		this.tags.next.className = "datepicker-pager datepicker-next";
 		this.tags.next.appendChild(FontAwesome.icon("angle-right"));
+		self.tags.prev.addEventListener("click", function(){ self.monthPrev(); }, false);
+		self.tags.next.addEventListener("click", function(){ self.monthNext(); }, false);
 		var selects = document.createElement("div");
 		selects.className = "datepicker-selects";
 		var months = document.createElement("div");
@@ -114,10 +117,86 @@ function datepicker(el) {
 		this.tags.picker.appendChild(arrow);
 		this.tags.picker.appendChild(inner);
 		this.tags.wrap.appendChild(this.tags.picker);
+		this.tags.weekdays = document.createElement("div");
+		this.tags.weekdays.className = "datepicker-weekdays";
+		var arr = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+		for (var i=0; i<7; i++) {
+			var wd = document.createElement("div");
+			wd.className = "datepicker-weekday";
+			wd.textContent = arr[i];
+			this.tags.weekdays.appendChild(wd);
+		}
+		inner.appendChild(this.tags.weekdays);
+		this.tags.calendar = document.createElement("div");
+		this.tags.calendar.className = "datepicker-calendar";
+		inner.appendChild(this.tags.calendar);
 	}
 	
 	this.renderCal = function() {
-		
+		var dates = this.getCalendarDates(this.active.y, this.active.m);
+		this.tags.calendar.innerHTML = "";
+		var week;
+		for (var i=0; i<dates.length; i++) {
+			if (i%7 == 0) {
+				if (week)
+					this.tags.calendar.appendChild(week);
+				week = document.createElement("div");
+				week.className = "datepicker-calendar-week";
+			}
+			var day = document.createElement("div");
+			day.className = "datepicker-calendar-day";
+			if (dates[i].m == this.active.m)
+				day.className+= " current-month";
+			if (dates[i].d == this.value.d && dates[i].m == this.value.m && this.active.y == this.value.y)
+				day.className+= " active";
+			day.textContent = dates[i].d;
+			week.appendChild(day);
+			(function(self, tag, y, m, d) {
+				tag.addEventListener("click", function(){ self.chooseDate(y, m, d); }, false);
+			}(this, day, dates[i].y, dates[i].m, dates[i].d));
+		}
+		this.tags.calendar.appendChild(week);
+	}
+	
+	this.getCalendarDates = function(y, m) {
+		var date = new Date(y+"-"+this.addZero(m)+"-01");
+		var dotw = this.getDay(date);
+		var days = [];
+		if (dotw != 0) {
+			var d = new Date(date.getTime()-1000*60*60*24*dotw);
+			for (var i=0, day = d.getDate(); i<dotw; i++) {
+				days.push({
+					y: d.getFullYear(),
+					m: d.getMonth()+1,
+					d: day+i
+				});
+			}
+		}
+		var d = new Date(date.getTime());
+		while(d.getMonth()+1 == m) {
+			days.push({
+				y: d.getFullYear(),
+				m: d.getMonth()+1,
+				d: d.getDate()
+			});
+			d.setTime(d.getTime()+1000*60*60*24);
+		}
+		dotw = this.getDay(d);
+		if (dotw != 0) {
+			for (var i=7; i>dotw; i--) {
+				days.push({
+					y: d.getFullYear(),
+					m: d.getMonth()+1,
+					d: d.getDate()
+				});
+				d.setTime(d.getTime()+1000*60*60*24);
+			}
+		}
+		return days;
+	}
+	
+	this.getDay = function(date) {
+		return (date.getDay()+6)%7;
 	}
 	
 	this.selectToggle = function(key) {
@@ -127,12 +206,49 @@ function datepicker(el) {
 			this.selectOpen(key);
 	}
 	this.selectOpen = function(key) {
+		if (this.selectActive)
+			this.selectClose(this.selectActive);
 		this.tags[key].parentNode.addClass("active");
+		this.selectActive = key;
 	}
 	this.selectClose = function(key) {
 		this.tags[key].parentNode.removeClass("active");
+		if (key == this.selectActive)
+			this.selectActive = null;
 	}
 	
+	this.monthPrev = function() {
+		if (this.active.m == 1) {
+			this.active.y--;
+			this.active.m = 12;
+			this.tags.year.textContent = this.active.y;
+		}
+		else {
+			this.active.m--;
+		}
+		this.tags.month.textContent = this.months[this.active.m-1];
+		this.renderCal();
+	}
+	this.monthNext = function() {
+		if (this.active.m == 12) {
+			this.active.y++;
+			this.active.m = 1;
+			this.tags.year.textContent = this.active.y;
+		}
+		else {
+			this.active.m++;
+		}
+		this.tags.month.textContent = this.months[this.active.m-1];
+		this.renderCal();
+	}
+	
+	this.chooseDate = function(y, m, d) {
+		this.close();
+		this.value.y = y;
+		this.value.m = m;
+		this.value.d = d;
+		this.tags.input.value = y+"-"+this.addZero(m)+"-"+this.addZero(d);
+	}
 	this.chooseMonth = function(m) {
 		this.active.m = m;
 		this.selectClose("month");
@@ -156,14 +272,33 @@ function datepicker(el) {
 		return (this.tags.wrap.className.match("datepicker-open") ? true : false);
 	}
 	this.open = function() {
+		this.renderCal();
 		this.tags.wrap.addClass("datepicker-open");
 	}
 	this.close = function() {
 		this.tags.wrap.removeClass("datepicker-open");
 	}
+	
+	this.addZero = function(val) {
+		if (val < 10)
+			return "0"+val;
+		return val;
+	}
 
 	this.windowClick = function(e) {
-		var el = e.target;
+		this.checkWrap(e.target);
+		this.checkSelect(e.target);
+	}
+	this.checkSelect = function(el) {
+		if (!this.selectActive)
+			return;
+		for (var i=0; el  && i<8; el = el.parentNode, i++) {
+			if (el == this.tags[this.selectActive].parentNode)
+				return;
+		}
+		this.selectClose(this.selectActive);
+	}
+	this.checkWrap = function(el) {
 		for (var i=0; el  && i<8; el = el.parentNode, i++) {
 			if (el == this.tags.wrap)
 				return;
