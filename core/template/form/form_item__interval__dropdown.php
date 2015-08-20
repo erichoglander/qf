803144a1d5b804a424f25c97fde9abfaff6889eb
= <eric@addcream.se>
2015-08-20 15:54:46 +0200

    Files...

---
+++ core/template/form/form_item__interval__dropdown.php
@@ -0,0 +1,38 @@
+<?=$prefix?>
+<div class="<?=$item_class?> interval-dropdown">
+<?php if (!empty($label)) { ?>
+	<label class="form-label form-textfield" onclick="intervalToggle(this)">
+		<?=$label?><?=FontAwesome\Icon("angle-down")?>
+	</label>
+<?php } ?>
+<?php 
+if ($items !== null) {
+	foreach ($items as $item) 
+		print $item;
+}
+else {
+?>
+	<div class="form-input">
+		<?=$input_prefix?>
+		<?php if (!empty($icon)) { ?>
+			<?=FontAwesome\Icon($icon, "form-icon-addon")?>
+		<?php } ?>
+		<?=$input?>
+		<?php if (!empty($error)) { ?>
+			<?=FontAwesome\Icon("times", "form-icon-feedback form-icon-error")?>
+		<?php } ?>
+		<?=$input_suffix?>
+	</div>
+	<?php if (!empty($error)) { ?>
+		<div class="form-input-error"><?=$error?></div>
+	<?php } ?>
+<?php
+}
+?>
+	<?=$delete_button?>
+	<?=$add_button?>
+	<div class="form-item-loader">
+		<?=FontAwesome\Icon("spinner", "fa-pulse")?>
+	</div>
+</div>
+<?=$suffix?>
