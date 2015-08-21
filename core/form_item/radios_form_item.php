837dac360510c964cb046afb35f73a58032aefc8
= <eric@addcream.se>
2015-08-21 10:21:14 +0200

    Radio form items.

---
+++ core/form_item/radios_form_item.php
@@ -0,0 +1,12 @@
+<?php
+class Radios_FormItem_Core extends FormItem {
+
+	protected function options() {
+		return $this->options;
+	}
+	
+	protected function inputType() {
+		return "radio";
+	}
+
+};
--- core/template/form/form_input__radios.php
+++ core/template/form/form_input__radios.php
@@ -1,3 +1,8 @@
 <?php foreach ($options as $key => $val) { ?>
-	<label class="radio-label"><input <?=$attributes?> value="<?=$key?>"<?=($key == $value && strlen($key) == strlen($value) ? " checked" : "")?>> <?=$val?></label>
+	<label class="form-label radio-label">
+		<input <?=$attributes?> value="<?=$key?>"<?=($key == $value && strlen($key) == strlen($value) ? " checked" : "")?>>
+		<?=FontAwesome\Icon("circle radio-icon-unchecked")?>
+		<?=FontAwesome\Icon("check-circle radio-icon-checked")?>
+		<?=$val?>
+	</label>
 <?php } ?>
