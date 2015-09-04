d370febe5b9934e261d14fe2d089af903dda04fb
eric-addcream <eric@addcream.se>
2015-09-04 11:11:10 +0200

    First version of installation script. Untested!

---
+++ install.php
@@ -0,0 +1,258 @@
+<?php
+require_once("core/inc/bootstrap.php");
+
+if (!IS_CLI)
+	die("Must be run through cli.");
+
+if (!$Db->connected)
+	die("Could not connect to database.\n");
+
+if ($Db->numRows("SHOW TABLES LIKE 'alias'"))
+	die("System already installed.\n");
+
+$sql = "
+--
+-- Table structure for table `alias`
+--
+
+CREATE TABLE IF NOT EXISTS `alias` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `status` tinyint(4) DEFAULT '1',
+  `created` int(10) unsigned DEFAULT NULL,
+  `updated` int(10) unsigned DEFAULT NULL,
+  `path` varchar(256) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `alias` varchar(256) COLLATE utf8_swedish_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `path` (`path`(255)),
+  KEY `alias` (`alias`(255)),
+  KEY `status` (`status`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache`
+--
+
+CREATE TABLE IF NOT EXISTS `cache` (
+  `name` varchar(128) COLLATE utf8_swedish_ci NOT NULL,
+  `data` longblob,
+  `expire` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `content`
+--
+
+CREATE TABLE IF NOT EXISTS `content` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `sid` int(11) DEFAULT NULL,
+  `lang` varchar(2) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `title` varchar(256) COLLATE utf8_swedish_ci NOT NULL,
+  `data` longblob NOT NULL,
+  `config` longblob NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `created` int(10) unsigned NOT NULL DEFAULT '0',
+  `updated` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `lang` (`lang`),
+  KEY `sid` (`sid`),
+  KEY `status` (`status`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file`
+--
+
+CREATE TABLE IF NOT EXISTS `file` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) COLLATE utf8_swedish_ci NOT NULL,
+  `uri` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `extension` varchar(12) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `dir` enum('public','private') COLLATE utf8_swedish_ci NOT NULL DEFAULT 'public',
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `created` int(10) unsigned NOT NULL DEFAULT '0',
+  `updated` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `status` (`status`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `l10n_string`
+--
+
+CREATE TABLE IF NOT EXISTS `l10n_string` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `sid` int(10) unsigned NOT NULL DEFAULT '0',
+  `lang` varchar(2) COLLATE utf8_swedish_ci NOT NULL,
+  `string` longtext COLLATE utf8_swedish_ci NOT NULL,
+  `input_type` enum('code','manual','import') COLLATE utf8_swedish_ci NOT NULL DEFAULT 'code',
+  `created` int(10) unsigned NOT NULL DEFAULT '0',
+  `updated` int(10) unsigned DEFAULT '0',
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `sid` (`sid`),
+  KEY `lang` (`lang`),
+  KEY `status` (`status`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `language`
+--
+
+CREATE TABLE IF NOT EXISTS `language` (
+  `lang` varchar(2) COLLATE utf8_swedish_ci NOT NULL,
+  `title` varchar(64) COLLATE utf8_swedish_ci NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`lang`),
+  KEY `status` (`status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log`
+--
+
+CREATE TABLE IF NOT EXISTS `log` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `type` enum('info','success','warning','error') COLLATE utf8_swedish_ci NOT NULL DEFAULT 'info',
+  `category` varchar(64) COLLATE utf8_swedish_ci NOT NULL,
+  `text` longtext COLLATE utf8_swedish_ci NOT NULL,
+  `data` longblob,
+  `created` int(10) unsigned NOT NULL,
+  `ip` varchar(64) COLLATE utf8_swedish_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `created` (`created`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_attempt`
+--
+
+CREATE TABLE IF NOT EXISTS `login_attempt` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `created` int(10) unsigned NOT NULL DEFAULT '0',
+  `ip` varchar(64) COLLATE utf8_swedish_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ip` (`ip`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `redirect`
+--
+
+CREATE TABLE IF NOT EXISTS `redirect` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  `created` int(10) unsigned NOT NULL DEFAULT '0',
+  `updated` int(10) unsigned NOT NULL DEFAULT '0',
+  `source` varchar(256) COLLATE utf8_swedish_ci NOT NULL,
+  `target` varchar(512) COLLATE utf8_swedish_ci NOT NULL,
+  `code` enum('301','302','303','307') COLLATE utf8_swedish_ci NOT NULL DEFAULT '301',
+  PRIMARY KEY (`id`),
+  KEY `status` (`status`),
+  KEY `source` (`source`(255))
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) COLLATE utf8_swedish_ci NOT NULL,
+  `machine_name` varchar(32) COLLATE utf8_swedish_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `machine_name` (`machine_name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) COLLATE utf8_swedish_ci NOT NULL,
+  `email` varchar(128) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `status` tinyint(4) DEFAULT '1',
+  `created` int(11) NOT NULL DEFAULT '0',
+  `updated` int(11) NOT NULL DEFAULT '0',
+  `login` int(11) NOT NULL DEFAULT '0',
+  `salt` varchar(128) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `pass` varchar(130) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `reset` varchar(128) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `reset_time` int(10) unsigned DEFAULT '0',
+  `email_confirmation` varchar(128) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `email_confirmation_time` int(10) unsigned DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`),
+  KEY `email` (`email`),
+  KEY `status` (`status`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `user_id` int(10) unsigned NOT NULL,
+  `role_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `role_id` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `variable`
+--
+
+CREATE TABLE IF NOT EXISTS `variable` (
+  `name` varchar(128) COLLATE utf8_swedish_ci NOT NULL,
+  `data` longblob,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD CONSTRAINT `user_role_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+";
+
+print "Constructing database... ";
+if (!$Db->query($sql))
+	die("Failed\n");
+print "OK\n";
+
+$Variable = newClass("Variable");
+$Variable->set("core_update", 2);
+
+print "Installation successful!\n";
