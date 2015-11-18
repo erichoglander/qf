057c71650f971d70998b3dc85bb2491bf56f82e0
eric-addcream <eric@addcream.se>
2015-11-18 22:51:12 +0100

    Documented the cache class.

--- core/class/cache.php
+++ core/class/cache.php
@@ -1,13 +1,37 @@
 <?php
+/**
+ * Contains the cache class
+ */
+
+/**
+ * Cache class
+ *
+ * Used to cache data in the database
+ * 
+ * @author Eric Höglander
+ */
 class Cache_Core {
 
+	/**
+	 * Database object
+	 * @var \Db_Core
+	 */
 	protected $Db;
 
-
+	/**
+	 * Constructor
+	 * @param \Db_Core $Db
+	 */
 	public function __construct($Db) {
 		$this->Db = $Db;
 	}
 	
+	/**
+	 * Get cached data
+	 * @param  string $name
+	 * @param  mixed  $def  Default value if the cache doesn't exist
+	 * @return mixed
+	 */
 	public function get($name, $def = null) {
 		$row = $this->Db->getRow("SELECT * FROM `cache` WHERE name = :name", [":name" => $name]);
 		if (!$row)
@@ -19,6 +43,12 @@ class Cache_Core {
 		return unserialize($row->data);
 	}
 
+	/**
+	 * Save data in database
+	 * @param string  $name
+	 * @param mixed   $data
+	 * @param int     $expire
+	 */
 	public function set($name, $data, $expire = 0) {
 		$values = [
 			"name" => $name,
@@ -32,16 +62,26 @@ class Cache_Core {
 			$this->Db->insert("cache", $values);
 	}
 
+	/**
+	 * Clears all caches
+	 */
 	public function clear() {
 		$this->Db->delete("cache");
 		$this->clearImagestyles();
 	}
 	
+	/**
+	 * Delete all public styled images
+	 */
 	public function clearImageStyles() {
 		$this->rmr(PUBLIC_PATH."/images/styles");
 	}
 	
-	
+
+	/**
+	 * Remove path and, if its a directory, everything in it
+	 * @param string $path
+	 */
 	protected function rmr($path) {
 		if (is_dir($path)) {
 			$files = array_diff(scandir($path), [".", ".."]);
