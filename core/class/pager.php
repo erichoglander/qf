d9d4e9c52723f32520684fbeb8103d09e3a9e0b5
eric-addcream <eric@addcream.se>
2015-11-18 23:01:19 +0100

    Documented the pager.

--- core/class/pager.php
+++ core/class/pager.php
@@ -1,19 +1,66 @@
 <?php
+/**
+ * Contains the pager class
+ */
+
+/**
+ * Pager class
+ *
+ * Renders a pager and keeps track of current page
+ * 
+ * @author Eric Höglander
+ */
 class Pager_Core {
 
+	/**
+	 * Current page
+	 * @var int
+	 */
 	public $page = 1;
+
+	/**
+	 * Posts Per Page or Items per page
+	 * @var int
+	 */
 	public $ppp = 30;
+
+	/**
+	 * How many pages to show before and after the current page
+	 * @var int
+	 */
 	public $span = 7;
+
+	/**
+	 * What param to fetch the current page from
+	 * @var string
+	 */
 	public $get = "page";
 	
+	/**
+	 * Number of items
+	 * @var int
+	 */
 	protected $num = 1;
+
+	/**
+	 * URL to serve as base for the pager links
+	 * @var string
+	 */
 	protected $url;
 
 
+	/**
+	 * Constructor
+	 */
 	public function __construct() {
 		$this->setUrl(url(REQUEST_URI));
 	}
 
+	/**
+	 * Get the url for a certain page
+	 * @param  int
+	 * @return string
+	 */
 	public function url($x) {
 		$regex = "/([\&|\?])".$this->get."\=[0-9]+/";
 		$url = $this->url;
@@ -24,18 +71,37 @@ class Pager_Core {
 		return $url;
 	}
 
+	/**
+	 * Renders a link for a page
+	 * @param  int    $x     The page number
+	 * @param  string $html  The html to fit inside the link tag
+	 * @param  string $class Any extra classes for the link tag
+	 * @return string
+	 */
 	public function tag($x, $html, $class = null) {
 		return '<a href="'.$this->url($x).'" class="page'.($class ? " ".$class : "").'">'.$html.'</a>';
 	}
 
+	/**
+	 * Get the start offset for the current page
+	 * @return int
+	 */
 	public function start() {
 		return ($this->page-1)*$this->ppp;
 	}
 	
+	/**
+	 * Setter for $url
+	 * @param string $url
+	 */
 	public function setUrl($url) {
 		$this->url = $url;
 	}
 
+	/**
+	 * Setter for $num
+	 * @param int $num
+	 */
 	public function setNum($num) {
 		$this->num = $num;
 		$this->pages = (int) max(1, ceil($this->num/$this->ppp));
@@ -46,6 +112,10 @@ class Pager_Core {
 			$this->page = $this->pages;
 	}
 	
+	/**
+	 * Render the complete pager
+	 * @return string
+	 */
 	public function render() {
 		if ($this->pages < 2)
 			return "";
