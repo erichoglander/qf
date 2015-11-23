88879e65551da3a4560d1225c73ea60bdf63821d
eric-addcream <eric@addcream.se>
2015-11-23 21:35:07 +0100

    Documented the update class.

--- core/class/update.php
+++ core/class/update.php
@@ -1,13 +1,36 @@
 <?php
+/**
+ * Contains update class
+ */
+
+/**
+ * Update class
+ *
+ * Serves as a base for updates
+ * 
+ * @author Eric Höglander
+ */
 class Update_Core {
 
+	/**
+	 * Database object
+	 * @var \Db_Core
+	 */
 	protected $Db;
 	
 
+	/**
+	 * Constructor
+	 * @param \Db_Core $Db
+	 */
 	public function __construct($Db) {
 		$this->Db = $Db;
 	}
 	
+	/**
+	 * Run the update
+	 * @return bool
+	 */
 	public function execute() {
 		return true;
 	}
