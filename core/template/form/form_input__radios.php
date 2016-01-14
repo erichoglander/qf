2e35c0d258d55866c9dfb7754516d8aa96e12047
eric-addcream <eric@addcream.se>
2016-01-14 13:26:51 +0100

    Added an inner span to radio labels like there is for checkboxes.

--- core/template/form/form_input__radios.php
+++ core/template/form/form_input__radios.php
@@ -3,6 +3,6 @@
 		<input <?=$attributes?> value="<?=$key?>"<?=($key == $value && strlen($key) == strlen($value) ? " checked" : "")?>>
 		<?=FontAwesome\Icon("circle radio-icon-unchecked")?>
 		<?=FontAwesome\Icon("check-circle radio-icon-checked")?>
-		<?=$val?>
+		<span class="label-inner"><?=$val?></span>
 	</label>
 <?php } ?>
