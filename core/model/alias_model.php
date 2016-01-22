3ca969894a5d3eb7d3bedd2ceae8ba19441b0e31
eric-addcream <eric@addcream.se>
2016-01-22 09:31:49 +0100

    Documented Alias model.

--- core/model/alias_model.php
+++ core/model/alias_model.php
@@ -1,6 +1,20 @@
 <?php
+/**
+ * Containers the alias model
+ */
+/**
+ * Alias model
+ * @author Eric Höglander
+ */
 class Alias_Model_Core extends Model {
 	
+	/**
+	 * Attempts to create an alias
+	 * @param  string $path
+	 * @param  string $alias
+	 * @param  bool   $unique If true, deletes any existing aliases for $path
+	 * @return \Alias_Entity_Core
+	 */
 	public function createAlias($path, $alias, $unique = false) {
 		$alias = $this->Io->filter($alias, "alias");
 		$row = $this->Db->getRow("
@@ -24,21 +38,42 @@ class Alias_Model_Core extends Model {
 		return $Alias;
 	}
 	
+	/**
+	 * Add a new alias
+	 * @see    editAlias
+	 * @param  array $values
+	 * @return bool
+	 */
 	public function addAlias($values) {
 		$Alias = $this->getEntity("Alias");
 		return $this->editAlias($Alias, $values);
 	}
 
+	/**
+	 * Edit an alias
+	 * @param  \Alias_Entity_Core $Alias
+	 * @param  array              $values
+	 * @return bool
+	 */
 	public function editAlias($Alias, $values) {
 		foreach ($values as $key => $value)
 			$Alias->set($key, $value);
 		return $Alias->save();
 	}
 
+	/**
+	 * Delete an alias
+	 * @param  \Alias_Entity_Core $Alias
+	 * @return bool
+	 */
 	public function deleteAlias($Alias) {
 		return $Alias->delete();
 	}
 
+	/**
+	 * Get all aliases
+	 * @return array
+	 */
 	public function getAliases() {
 		$rows = $this->Db->getRows("SELECT id FROM `alias` ORDER BY alias ASC");
 		$aliases = [];
@@ -47,6 +82,11 @@ class Alias_Model_Core extends Model {
 		return $aliases;
 	}
 	
+	/**
+	 * Database query for alias search
+	 * @param  array $values Search parameters
+	 * @return array Contains sql-query and vars
+	 */
 	public function listSearchQuery($values) {
 		$sql = "SELECT id FROM `alias`";
 		$vars = [];
@@ -56,10 +96,23 @@ class Alias_Model_Core extends Model {
 		}
 		return [$sql, $vars];
 	}
+	/**
+	 * Get number of aliases that matches the search
+	 * @see    listSearchQuery
+	 * @param  array $values   Search parameters
+	 * @return int
+	 */
 	public function listSearchNum($values = []) {
 		list($sql, $vars) = $this->listSearchQuery($values);
 		return $this->Db->numRows($sql, $vars);
 	}
+	/**
+	 * Search for aliases
+	 * @param  array $values Search parameters
+	 * @param  int   $start
+	 * @param  int   $stop
+	 * @return array
+	 */
 	public function listSearch($values = [], $start = 0, $stop = 30) {
 		list($sql, $vars) = $this->listSearchQuery($values);
 		$sql.= " ORDER BY path ASC LIMIT ".$start.", ".$stop;
