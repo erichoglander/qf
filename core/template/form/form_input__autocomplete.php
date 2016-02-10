1906c5ef028b471c65816e00cf3acc2343b8be15
eric-addcream <eric@addcream.se>
2016-02-10 14:44:28 +0100

    Add a html title to autocomplete previews.

--- core/template/form/form_input__autocomplete.php
+++ core/template/form/form_input__autocomplete.php
@@ -1,5 +1,5 @@
 <div class="autocomplete-preview form-textfield">
-	<div class="autocomplete-preview-title"><?=($value && array_key_exists("title", $value) ? $value["title"] : null)?></div>
+	<div class="autocomplete-preview-title" title="<?=($value && array_key_exists("title", $value) ? $value["title"] : null)?>"><?=($value && array_key_exists("title", $value) ? $value["title"] : null)?></div>
 	<div class="autocomplete-remove"><?=FontAwesome\Icon("times")?></div>
 </div>
 <input <?=$attributes?> value="<?=($value && array_key_exists("title", $value) ? $value["title"] : null)?>">
