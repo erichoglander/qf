e54fe119eb198e2a7cc0766ae63578e07fa7c803
eric-addcream <eric@addcream.se>
2016-04-06 14:59:00 +0200

    Fixed update 5 and 6 crashing when there were rows with deleted users.

--- core/update/update_5.php
+++ core/update/update_5.php
@@ -4,7 +4,7 @@ class Update_5_Core extends Update_Core {
   public function execute() {
     $qs = [
       "ALTER TABLE `login_attempt` CHANGE `user_id` `user_id` INT( 10 ) UNSIGNED NULL ;",
-      "UPDATE `login_attempt` SET user_id = NULL WHERE user_id = 0",
+      "UPDATE `login_attempt` LEFT JOIN `user` ON `user`.id = user_id SET user_id = NULL WHERE `user`.id IS NULL",
       "ALTER TABLE `login_attempt` ADD INDEX ( `user_id` ) ;",
       "ALTER TABLE `login_attempt` ADD FOREIGN KEY ( `user_id` ) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL ;",
     ];
--- core/update/update_6.php
+++ core/update/update_6.php
@@ -4,7 +4,7 @@ class Update_6_Core extends Update_Core {
   public function execute() {
     $qs = [
       "ALTER TABLE `log` CHANGE `user_id` `user_id` INT( 10 ) UNSIGNED NULL ;",
-      "UPDATE `log` SET user_id = NULL WHERE user_id = 0",
+      "UPDATE `log` LEFT JOIN `user` ON `user`.id = user_id SET user_id = NULL WHERE `user`.id IS NULL",
       "ALTER TABLE `log` ADD INDEX ( `user_id` ) ;",
       "ALTER TABLE `log` ADD INDEX ( `category` ) ;",
       "ALTER TABLE `log` ADD FOREIGN KEY ( `user_id` ) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE SET NULL ;",
