5397dfa62a758adefa8512a22b6dcdd906eb92ed
Eric Höglander <eric@addcream.se>
2016-06-30 10:56:38 +0200

    Fixed default value for autocompletes with TwoWord entities.

--- core/form_item/autocomplete_form_item.php
+++ core/form_item/autocomplete_form_item.php
@@ -51,7 +51,7 @@ class Autocomplete_FormItem_Core extends FormItem {
         return null;
       if (!$this->FormModel)
         $this->FormModel = $this->getModel("Form");
-      return xss($this->FormModel->autocompleteTitle($this->autocomplete_entity, $value["value"]));
+      return xss($this->FormModel->autocompleteTitle(snakeToPascal($this->autocomplete_entity), $value["value"]));
     }
     return null;
   }
