73cdc702cb864304d430d75482815d6dfadb37ee
Eric Höglander <eric@addcream.se>
2016-12-08 13:49:37 +0100

    Added --raw parameter to cli.php

--- cli.php
+++ cli.php
@@ -4,6 +4,11 @@
  *
  * Execute an uri through cli
  * Ex: php cli.php my-controller/my-action/arg1
+ *
+ * Flags:
+ *  --raw
+ *    Passes the $raw parameter to \ControllerFactory 
+ *    so the request ignores aliases and redirects
  * 
  * @author Eric Höglander
  */
@@ -13,10 +18,8 @@ require_once("core/inc/bootstrap.php");
 if (!IS_CLI)
   die("Must be run through cli");
 
-if (!$Db->connected)
-  die("Could not connect to database\n");
-
-$request_uri = ($_SERVER["argc"] < 2 ? "page/index" : $_SERVER["argv"][1]);
-$doc = $ControllerFactory->executeUri($request_uri);
+$raw = in_array("--raw", $_SERVER["argv"]);
+$request_uri = ($_SERVER["argc"] < 2 ? "page/index" : end($_SERVER["argv"]));
+$doc = $ControllerFactory->executeUri($request_uri, $raw);
 
 print $doc.PHP_EOL;
